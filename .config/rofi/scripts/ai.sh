25ef0d8656e439f88f4d8096ec36d8de55ef86a6
Hanssen0 <hanssen0@hanssen0.com>
2025-09-15 14:08:37 +0800

    feat(rofi): merge tools

--- .config/rofi/scripts/ai.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [[ "$@" == "gemini" ]]; then
-  coproc (~/.config/rofi/scripts/ask-gemini.zsh "$ROFI_INPUT")
-  exit 0
-fi
-
-echo -en "gemini\0display\x1fAsk Gemini\x1fpermanent\x1ftrue\x1ficon\x1fedit-find\n"
--- .config/rofi/scripts/tool.sh
+++ .config/rofi/scripts/tool.sh
@@ -2,14 +2,19 @@
 
 data=$ROFI_DATA
 
-if [[ "$@" == "tsx" ]]; then
+if [[ "$@" == "gemini" ]]; then
+  coproc (~/.config/rofi/scripts/ask-gemini.zsh "$ROFI_INPUT")
+  exit 0
+elif [[ "$@" == "tsx" ]]; then
 	input=$ROFI_INPUT
 
   source /usr/share/nvm/init-nvm.sh;
   data="$input = $(tsx -e "console.log($input)")<br/>$data"
 fi
 
+echo -en "gemini\0display\x1fAsk Gemini\x1fpermanent\x1ftrue\x1ficon\x1fedit-find\n"
 echo -en "tsx\0display\x1fRun Typescript\x1fpermanent\x1ftrue\x1ficon\x1faccessories-calculator\n"
+
 echo -en "\0data\x1f$data\n"
 echo "$data" | sed "s/<br\/>/\x0icon\x1fdialog-information\x1fpermanent\x1ftrue\x1fnonselectable\x1ftrue\n/g"
 

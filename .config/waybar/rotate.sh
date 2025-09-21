a17c17b8967e2e73d9a785dd0bb98259503057ea
Hanssen0 <hanssen0@hanssen0.com>
2025-09-21 21:09:48 +0800

    fix: side screen rotation

--- .config/waybar/rotate.sh
+++ .config/waybar/rotate.sh
@@ -11,13 +11,15 @@ mkfifo $cmd_pipe
 curr=0
 while true; do
   if [ $curr == 0 ]; then
-    echo "{\"alt\":\"to-portrait\",\"tooltip\":\"To Portrait\"}"
+    echo "{\"alt\":\"to-portrait\",\"tooltip\":\"Landscape\"}"
   else
-    echo "{\"alt\":\"to-landscape\",\"tooltip\":\"To Landscape\"}"
+    echo "{\"alt\":\"to-landscape\",\"tooltip\":\"Portrait\"}"
   fi
 
   read < $cmd_pipe
 
   curr=$(($curr==0?1:0))
-  hyprctl --batch "keyword monitorv2[DP-3]:transform $curr ; keyword device[wingcooltouch-wingcooltouch]:transform $curr" > /dev/null
+  hyprctl "keyword monitorv2[DP-3]:transform $curr" > /dev/null
+  hyprctl "keyword device[wingcooltouch-wingcooltouch]:transform $curr" > /dev/null
+  hyprctl "keyword device[wingcooltouch-wingcooltouch-1]:transform $curr" > /dev/null
 done
